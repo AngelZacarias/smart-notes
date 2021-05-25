470e99dfb12ee7a13d0ea476ea58e241a36e2e35
unknown <julio.gonzalez7589@alumnos.udg.mx>
2021-05-25 17:53:17 -0500

    Fix bug related to CRLF/LF and prettier

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const { MONGODB } = require('./config');
+const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
     type Query{
