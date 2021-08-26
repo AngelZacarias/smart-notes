9b81d576b779e697564d8e6af19e5095532bc654
Angel Zacarias <luis.zacariasm@alumnos.udg.mx>
2021-08-25 20:51:59 -0500

    Fisrts workaround of authentication validation in frontend

---
+++ Client/src/authentication/Authentication.jsx
@@ -0,0 +1,87 @@
+import { Route, Redirect } from 'react-router-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
+import AuthenticationService from './services/authenticationService';
+//import { UserContext } from './../hooks/UserContext';
+
+// This component routes to another component when its path prop matches the current URL, but before routing, asks for authentication. It must be used to request authentication to the components that you want to protect.
+class Authentication extends Route{
+    //Service to validate, generate and manage the Authentication
+    static authenticationService = new AuthenticationService();
+    //Context to be used to storage the user information trhough all the app
+    //static contextType = UserContext;
+
+    static propTypes = {
+        //This prop is used to specify the component that will be in charge of handling the response from w3id. This component's path prop must match with the redirect URI registered previously in the w3id SSO provisioner tool.
+        isCallBack: PropTypes.bool
+    }
+
+    static defaultProps = {
+        //Default value for isCallBack prop, only use it for the callback componet (redirection route) 
+        isCallBack: false
+    }
+
+    //State for this component
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoggedIn: false,
+            hasFinishedAuthentication: false
+        }
+    }
+
+    //Runs when the component is Loaded
+    async componentDidMount(){
+        if(this.props.isCallBack){
+            try{
+                //Finishes the authentication processs
+                console.log('Finishing authentication...');
+                //await Authentication.authenticationService.completeAuthentication();
+                
+                //Sends the credentials to the context
+                const userLoged = Authentication.authenticationService.getUser();
+                console.log(userLoged)
+                if(Authentication.authenticationService.isLoggedIn()){
+                    this.setState({ 
+                        isLoggedIn: false,
+                        hasFinishedAuthentication: false
+                    });
+                }
+                else{
+                    this.setState({ 
+                        isLoggedIn: true,
+                        hasFinishedAuthentication: true
+                    });
+                }
+                //const { setUserCredentials } = this.context;
+                //await setUserCredentials(userLoged.access_token, userLoged.id_token, userLoged.expires_at, userLoged.profile.emailAddress, userLoged.profile.name);
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+    }
+
+    //Render the component in props or redirect to authentication process
+    render(){
+        let componentContent;
+        if(!this.props.isCallBack){
+            if(Authentication.authenticationService.isLoggedIn()){
+                componentContent = super.render();
+            }
+            else{
+                Authentication.authenticationService.setOriginalUrl(this.props.path);
+                //Authentication.authenticationService.startAuthentication();
+                componentContent = <Redirect to={'/'} />;
+            }
+        }
+        else{
+            componentContent = (this.state.hasFinishedAuthentication) ? (
+                <Redirect to={Authentication.authenticationService.getOriginalUrl()} />
+            ) : super.render();
+        }
+        return componentContent;
+    }
+}
+
+export default Authentication;
---
+++ Client/src/authentication/services/authenticationService.js
@@ -0,0 +1,81 @@
+
+import jwt from 'jsonwebtoken';
+
+
+class AuthenticationService {
+    user = null;
+    ORIGINAL_URL_COOKIE = "originalUrl";
+
+    constructor() {
+        dotenv.config();
+        this.initializeUser();
+    }
+
+    async initializeUser() {
+        const token = localStorage.getItem("JWT_TOKEN");
+        if (token) {
+            try {
+                this.getClaimsuser = jwt.verify(token, process.env.JWT_KEY);
+            } catch (err) {
+                throw new AuthenticationError("Invalid/Expired token");
+            }
+        }
+        throw new Error(
+            "Authentication failed. Make sure you have provided an Authentication token in correct format"
+        );
+    }
+
+    /**
+     * Get the previously authenticated user's info. 
+     * @returns {Object} An object object with user's info.
+     */
+    getUser() {
+        return this.user;
+    }
+
+    /**
+      * Validates if the user has been previously authenticated succesfully.
+      * 
+      * @returns {boolean} true, if the user has been authenticated successfully  and if the user's session hasn't exipred yet,
+      * otherwise false.
+      */
+    isLoggedIn() {
+        return this.user != null && !this.user.expired;
+    }
+
+   
+    /**
+     * Begins the authentication process, this method redirects to w3id login page.
+     */
+    startAuthentication() {
+        return this.userManager.signinRedirect();
+    }
+
+    /**
+     * Finishes the authentication process, this method must be called once w3id redirects to the registered callback url, in order to save the user data.
+     */
+    async completeAuthentication() {
+        this.user = await this.userManager.signinRedirectCallback();
+    }
+
+    /**
+     * This method is used to store the original url that the user requested before being redirected to w3id login page, and 
+     * navigate to it once authentication finishes.
+     * 
+     * @param originalUrl The original endpoint that the user requested before being redirected to w3id login page.
+     */
+    setOriginalUrl(originalUrl) {
+        localStorage.setItem(this.ORIGINAL_URL_COOKIE, originalUrl);
+    }
+
+    /**
+     * This method is useful after {@link this.completeAuthentication} method is called, in order to get the 
+     * url that the user requested before being redirected to w3id login page, and then, get the user redirected to
+     * it.
+     */
+    getOriginalUrl() {
+        return localStorage.getItem(this.ORIGINAL_URL_COOKIE);
+    }
+}
+
+export default AuthenticationService;
