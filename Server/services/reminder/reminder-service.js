cd6da4dece9b6742e8f1a927078b96fae0ebf97d
Julio González Martín <jagonzalez@getinsoft.com>
2021-09-20 16:11:28 -0500

    Set email and transporter configuration. Install email-templates and nodemailer

--- Server/services/reminder/reminder-service.js
+++ Server/services/reminder/reminder-service.js
@@ -1,5 +1,14 @@
+import { sendMail } from "../email/email-sender";
 
-function remindOfPendingTasks() {
+async function remindOfPendingTasks() {
+  // To send an email uncomment next section
+  // await sendMail("improgrades@gmail.com", "test", {
+  //   taskName: "Resumen del capítulo 4 de Programación Competitiva",
+  //   userName: "Julio González",
+  //   deadline: "21 de septiembre a las 3:00 pm",
+  //   clientOrigin: process.env.CLIENT_ORIGIN,
+  //   urlViewRequest: "www.google.com"
+  // });
   console.log("Sistema de recordatorio funcionando!");
 }
 
